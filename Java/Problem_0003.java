f20d2f052bf0f45942c2c5d7f550ee993369dc99
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-06-08 12:25:22 -0400

    Solved first two problems from Project Euler

---
+++ Java/Problem_0003.java
@@ -0,0 +1,15 @@
+package solutions;
+
+public final class Problem_0003 {
+
+	/*
+	 * The prime factors of 13195 are 5, 7, 13 and 29.
+	 * What is the largest prime factor of the number 600851475143 ?
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}	
+}

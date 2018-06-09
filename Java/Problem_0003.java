db3a4f0faecd7c0e136a926a3ef5e3e7b0f25842
Yashas Lokesh <yashas.lokesh@gmail.com>
2018-06-09 16:20:47 -0400

    Implemented question 3, created class for problem 4

--- Java/Problem_0003.java
+++ Java/Problem_0003.java
@@ -3,13 +3,26 @@ package solutions;
 public final class Problem_0003 {
 
 	/*
-	 * The prime factors of 13195 are 5, 7, 13 and 29.
-	 * What is the largest prime factor of the number 600851475143 ?
+	 * The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime
+	 * factor of the number 600851475143 ?
 	 * 
 	 */
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	}	
+		long num = 600_851_475_143L;
+		while (num != smallestFactor(num)) {
+			num /= smallestFactor(num);
+		}
+		System.out.println(num);
+	}
+
+	private static long smallestFactor(long num) {
+		for (int i = 2; i < Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				return i;
+			}
+		}
+		return num;
+	}
 }
